c829e28c7f90f9c3d645902c5fb93b36513531f5
Wallis <wallis.sl@outlook.com>
2026-02-23 17:21:38 -0300

    criação do layout

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('Hello World');
+
+let artDes = document.getElementById('artDes');
+let artDev = document.getElementById('artDev');
+let header = document.getElementById('header');
+let linke = document.getElementsByClassName('linke');
+
+artDes.addEventListener('click', cliDes);
+artDev.addEventListener('click', cliDev);
+header.addEventListener('mousemove', mouHea);
+//linke.addEventListener('mousemove', linkeAcess);
+
+function cliDes(){
+    alert('Portfólio de Design | Behance');
+}
+
+function cliDev(){
+    alert('Repositório | GitHub ')
+}
+
+function clickCur(){
+    alert('Currículo | Linkedin')
+}
+
+function mouHea(){
+    header.style.backgroundColor="#6729ca";
+
+}
+
+header.addEventListener('mouseout', ()=>{
+    header.style.backgroundColor="#000";
+   
+});
+ 
+function linkeAcess(){
+    linke[0].style.color="#a779ff";
+}
+
+linke[0].addEventListener('mouseout', ()=>{
+    linke[0].style.color="#686363";
+});
+
